e3b2266c741ad4419e124f3ba50bc832e3464d1e
Mahmoud Samir <menatosamir55@gmail.com>
2025-04-26 02:02:35 +0300

    thank you template & handel post request

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        print(username)
+        return HttpResponseRedirect("/thank-you")
+    
     return render(request, "reviews/review.html", {})
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html") 
