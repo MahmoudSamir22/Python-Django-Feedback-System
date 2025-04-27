56266e9ab227a919f72749dbc83a24da75804915
Mahmoud Samir <menatosamir55@gmail.com>
2025-04-27 09:48:46 +0300

    use list and detail view

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from .models import Review
 # Create your views here.
 
 class ReviewView(View):
@@ -29,4 +31,21 @@ class ThankYouView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs) 
         context['message'] = "this works"
+        return context
     
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name= "reviews"
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+    
+
+    
+class ReviewDetailsView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
+    context_object_name = "review"
